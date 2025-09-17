58588887c870e55751720445c0767c7b5cb1fc0b
yuruili-cloud <yuruili@students.calarts.edu>
2025-09-16 20:44:09 -0500

    initial commit

---
+++ sketch.js
@@ -0,0 +1,9 @@
+function setup() {
+  // Create a canvas that fills the entire browser window
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+background(255,0,0);
+circle(130,123,123);
+}
