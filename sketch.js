e2d59b338172121ffe6d61f453cd683c546d7a00
yuruili-cloud <yuruili@students.calarts.edu>
2025-09-23 15:16:03 -0500

    Add files via upload
    yuruili

--- sketch.js
+++ sketch.js
@@ -4,6 +4,11 @@ function setup() {
 }
 
 function draw() {
-background(255,0,0);
-circle(130,123,123);
+ background(255,0,50,60);
+ circle(123,234,500);
+circle(mouseX,mouseY,500);
+
 }
+
+//circle() command makes a circle
+//color is between 0-255
