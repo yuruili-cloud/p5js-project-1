b90076a7a88c82d5ce1cc66b10377134e371e842
yuruili-cloud <yuruili@students.calarts.edu>
2025-12-10 10:25:58 +0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,14 +1,20 @@
-function setup() {
-  // Create a canvas that fills the entire browser window
-  createCanvas(windowWidth, windowHeight);
-}
 
-function draw() {
- background(255,0,50,60);
- circle(123,234,500);
-circle(mouseX,mouseY,500);
 
-}
 
-//circle() command makes a circle
-//color is between 0-255
+ 
+ 
+
+
+
+
+
+
+
+
+
+
+ 
+ 
+
+
+
